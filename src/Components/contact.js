e864b295480034f714b200ad7360625eb35a4af9
Matthew Starr <buylowsellhigh@me.com>
2020-03-15 00:34:09 -0400

    Created Components folder with contact.js file inside

---
+++ src/Components/contact.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+    const Contacts = ({ contacts }) => {
+      console.log()
+      return (
+        <div>
+          <center><h1>Contact List</h1></center>
+          {contacts.map((contact) => (
+            <div className="card">
+              <div className="card-body">
+                <h4 className="card-title">{contact.name}</h4>
+                <h6 className="card-title">{contact.email}</h6>
+                <h6 className="card-title">{contact.phone}</h6>
+                <h6 className="card-title">{contact.company.name}</h6>
+
+                
+               
+              </div>
+            </div>
+          ))}
+        </div>
+      )
+    };
+
+    export default Contacts
